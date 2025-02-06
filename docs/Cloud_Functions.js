49c425c7da193dfd3f9c9deca73afc548e45ff70
YYBartT <bteunis@opera.com>
2025-02-06 14:11:41 +0100

    docs(general): added missing documentation for FirebaseCloudFunctions_Call()

--- docs/Cloud_Functions.js
+++ docs/Cloud_Functions.js
@@ -1,3 +1,48 @@
+// Functions
+
+/**
+ * @func FirebaseCloudFunctions_Call
+ * @desc This function calls the cloud function with the given name and the given parameters.
+ * 
+ * @param {string} function The name of the function to call
+ * @param {struct|array} params The parameters to pass to the function
+ * @param {real} [timeout] An optional timeout value in seconds. Defaults to 0.
+ * 
+ * @event social
+ * @desc Triggered on a successful function call:
+ * @member {string} type The string `"FirebaseCloudFunctions_Call"`
+ * @member {real} listener The async request ID
+ * @member {real} status The status code of the call
+ * @member {string} value The data returned by the function call
+ * @event_end
+ * 
+ * @event social
+ * @desc Triggered when an error occurred:
+ * @member {string} type The string `"FirebaseCloudFunctions_Call"`
+ * @member {real} listener The async request ID
+ * @member {real} status The status code of the call
+ * @member {string} errorMessage A message describing the error that occurred
+ * @event_end
+ * 
+ * @example
+ * ```gml
+ * async_id = FirebaseCloudFunctions_Call("my_cloud_function", {text: "Hello!", number: 100});
+ * ```
+ * 
+ * ```gml
+ * /// Async Social Event
+ * if (async_load[? "listener"] != async_id) { exit; }
+ * 
+ * if (!ds_map_exists(async_load, "value")) { exit; }
+ * 
+ * show_debug_message($"Result: {async_load[? "value"]}");
+ * ```
+ * 
+ * @func_end
+ */
+
+// Modules
+
 /**
  * @module cloud_functions
  * @title Cloud Functions Overview
@@ -15,6 +60,11 @@
  * * ${page.guides_cloud_storage#Firebase_Console}
  * * ${page.guides_cloud_storage#Platform_Setup}
  * 
+ * @section_end
+ * 
+ * @section_func Functions
+ * @desc These are the functions in this module:
+ * @ref FirebaseCloudFunctions_Call
  * @section_end
  * 
  * @module_end
